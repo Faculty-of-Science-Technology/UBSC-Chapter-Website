8a77014a27ae072fb44ddd99cd95500b609c8ac9
Immanuel Daviel A. Garcia <34188635+AlexDev404@users.noreply.github.com>
2025-02-24 23:46:04 +0000

    Enforce authorization check in dashboard layout to include parent directory authentication

---
+++ src/routes/dashboard/+layout.server.ts
@@ -0,0 +1,9 @@
+import { error } from '@sveltejs/kit';
+import type { LayoutServerLoad } from './$types';
+
+export const load: LayoutServerLoad = async ({ locals, parent }) => {
+	const authenticated = (await parent()).authenticated;
+	if (!locals.user && !authenticated) {
+		throw error(401, 'Unauthorized');
+	}
+};
