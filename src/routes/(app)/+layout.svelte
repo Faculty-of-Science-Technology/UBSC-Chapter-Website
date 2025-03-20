2a0df5521ca7832bd8931cac60838e51f58b13e2
AlexDev404 <alexdev404@users.noreply.github.com>
2025-03-20 03:37:33 -0600

    feat: Break the layout a bit for the landing page

---
+++ src/routes/(app)/+layout.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import Navigation from '$lib/components/Navigation.svelte';
+	let props = $props();
+	const data = props.data;
+	const authenticated = data.authenticated;
+</script>
+
+<Navigation {authenticated} />
+
+{@render props.children()}
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,15 +1,12 @@
+
 <script lang="ts">
-	import Navigation from '$lib/components/Navigation.svelte';
 	import { Toaster } from '$lib/components/vendor/ui/sonner';
 	import { i18n } from '$lib/i18n';
 	import { ParaglideJS } from '@inlang/paraglide-sveltekit';
 	import '../app.css';
 	let props = $props();
-	const data = props.data;
-	const authenticated = data.authenticated;
 </script>
 
-<Navigation {authenticated} />
 <ParaglideJS {i18n}>
 	<Toaster />
 	{@render props.children()}
