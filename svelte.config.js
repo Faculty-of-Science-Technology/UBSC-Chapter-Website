8445c06504a832bcbc73da5329dfcb1c233d5531
AlexDev404 <alexdev404@users.noreply.github.com>
2025-04-11 20:30:18 -0600

    feat: Switch Svelte adapter based on DEVELOPMENT environment variable

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,10 @@
-import adapter from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-node';
+import { default as adapter_vercel } from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+// Determine which adapter to use based on DEVELOPMENT env variable
+const isDevelopment = process.env.DEVELOPMENT === 'true';
+const selectedAdapter = isDevelopment ? adapter_vercel : adapter;
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -13,7 +18,7 @@ const config = {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: selectedAdapter()
 	}
 };
 
