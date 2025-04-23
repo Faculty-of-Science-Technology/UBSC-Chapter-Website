8de7818ee24e335df1ed6e3399f732f5f48af11a
AlexDev404 <AlexDev404@users.noreply.github.com>
2025-04-23 15:15:47 -0600

    Implement dynamic database drivers based on build type

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -1,7 +1,24 @@
-import { DATABASE_URL } from '$env/static/private';
+import { DATABASE_URL, DATABASE_URL_UNPOOLED, IS_DEVELOPMENT } from '$env/static/private';
 import { neon } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-http';
+import { drizzle as drizzle_prod } from 'drizzle-orm/node-postgres';
 import * as schema from './schema';
+
 if (!DATABASE_URL) throw new Error('DATABASE_URL is not set');
-const client = neon(DATABASE_URL);
-export const db = drizzle(client, { schema: { ...schema } });
+if (!DATABASE_URL_UNPOOLED) throw new Error('DATABASE_URL_UNPOOLED is not set');
+
+const development = IS_DEVELOPMENT === 'true';
+
+const connectionString = development ? DATABASE_URL : DATABASE_URL_UNPOOLED;
+
+const db = (() => {
+	if (development) {
+		const client = neon(connectionString);
+		return drizzle(client, { schema: { ...schema } });
+	} else {
+		return drizzle_prod(connectionString, { schema: { ...schema } });
+	}
+})();
+
+export { db };
+
