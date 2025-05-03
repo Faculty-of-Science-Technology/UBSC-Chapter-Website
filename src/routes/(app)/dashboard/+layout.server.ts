286ec9e3e28794df36069f8c26f0988b2750af0c
Immanuel Daviel A. Garcia <34188635+AlexDev404@users.noreply.github.com>
2025-05-03 23:39:05 +0000

    feat: Enhance authentication checks and error handling in dashboard layout and person page

--- src/routes/(app)/dashboard/+layout.server.ts
+++ src/routes/(app)/dashboard/+layout.server.ts
@@ -1,7 +1,8 @@
+import { PLATFORM_PRIVATE_ACCOUNT_MODE } from '$env/static/private';
 import { error } from '@sveltejs/kit';
 import type { LayoutServerLoad } from './$types';
 
-export const load: LayoutServerLoad = async ({ locals, parent }) => {
+export const load: LayoutServerLoad = async ({ locals, parent, request }) => {
 	const authenticated = (await parent()).authenticated;
 	if (locals.user && authenticated) {
 		return {
@@ -9,5 +10,21 @@ export const load: LayoutServerLoad = async ({ locals, parent }) => {
 			authenticated
 		};
 	}
-	throw error(401, '✗ Unauthorized');
+
+	if (!request.url.includes('/dashboard/people/')) {
+		throw error(401, '✗ Unauthorized');
+	}
+	
+	if (JSON.parse(PLATFORM_PRIVATE_ACCOUNT_MODE) === false) {
+		return {
+			user: {
+				AccountType: null,
+				Email: null,
+				Id: null
+			},
+			authenticated: false
+		};
+	} else {
+		throw error(401, '✗ Unauthorized');
+	}
 };
