c0f2098277632ebc8466b9483b4ec4ff9e7546bb
AlexDev404 <34188635+AlexDev404@users.noreply.github.com>
2025-12-31 06:36:34 -0600

    fix: Convert most visible icons to use lucide

--- src/routes/dashboard/members/+page.svelte
+++ src/routes/dashboard/members/+page.svelte
@@ -4,6 +4,7 @@
 	import { Label } from '$lib/components/vendor/ui/label';
 	import * as Select from '$lib/components/vendor/ui/select';
 	import HeroText from '$lib/templates/landing/widgets/hero-text.svelte';
+	import { CalendarIcon, CircleCheck, ShieldCheck, Users } from 'lucide-svelte';
 	import type { PageProps } from './$types';
 
 	const { data }: PageProps = $props();
@@ -125,19 +126,7 @@
 			<div class="p-5">
 				<div class="flex items-center">
 					<div class="flex-shrink-0">
-						<svg
-							class="h-6 w-6 text-muted"
-							fill="none"
-							viewBox="0 0 24 24"
-							stroke="currentColor"
-						>
-							<path
-								stroke-linecap="round"
-								stroke-linejoin="round"
-								stroke-width="2"
-								d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197m13.5-9a2.5 2.5 0 11-5 0 2.5 2.5 0 015 0z"
-							/>
-						</svg>
+						<Users class="h-6 w-6 text-primary" />
 					</div>
 					<div class="ml-5 w-0 flex-1">
 						<dl>
@@ -153,19 +142,7 @@
 			<div class="p-5">
 				<div class="flex items-center">
 					<div class="flex-shrink-0">
-						<svg
-							class="h-6 w-6 text-green-400"
-							fill="none"
-							viewBox="0 0 24 24"
-							stroke="currentColor"
-						>
-							<path
-								stroke-linecap="round"
-								stroke-linejoin="round"
-								stroke-width="2"
-								d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
-							/>
-						</svg>
+						<CircleCheck class="h-6 w-6 text-green-400" />
 					</div>
 					<div class="ml-5 w-0 flex-1">
 						<dl>
@@ -181,14 +158,7 @@
 			<div class="p-5">
 				<div class="flex items-center">
 					<div class="flex-shrink-0">
-						<svg class="h-6 w-6 text-sky-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-							<path
-								stroke-linecap="round"
-								stroke-linejoin="round"
-								stroke-width="2"
-								d="M9 12l2 2 4-4m5.618-4.016A11.955 11.955 0 0112 2.944a11.955 11.955 0 01-8.618 3.04A12.02 12.02 0 003 9c0 5.591 3.824 10.29 9 11.622 5.176-1.332 9-6.03 9-11.622 0-1.042-.133-2.052-.382-3.016z"
-							/>
-						</svg>
+						<ShieldCheck class="h-6 w-6 text-primary" />
 					</div>
 					<div class="ml-5 w-0 flex-1">
 						<dl>
@@ -204,19 +174,7 @@
 			<div class="p-5">
 				<div class="flex items-center">
 					<div class="flex-shrink-0">
-						<svg
-							class="h-6 w-6 text-blue-400"
-							fill="none"
-							viewBox="0 0 24 24"
-							stroke="currentColor"
-						>
-							<path
-								stroke-linecap="round"
-								stroke-linejoin="round"
-								stroke-width="2"
-								d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"
-							/>
-						</svg>
+						<CalendarIcon class="h-6 w-6 text-primary" />
 					</div>
 					<div class="ml-5 w-0 flex-1">
 						<dl>
